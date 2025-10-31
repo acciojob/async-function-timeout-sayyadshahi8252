d87f28e493632facbc5adc80332c8b1894728935
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-31 11:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let button=document.getElementById("btn")
+btn.addEventListener("click",async()=>{
+	let textdata=document.getElementById("text").value.trim();
+	let numberdata=document.getElementById("delay").value.trim();
+	let output=document.getElementById("output")
+	await new Promise(resolve => setTimeout(resolve, numberdata));
+  output.innerText = textdata;
+
+})
